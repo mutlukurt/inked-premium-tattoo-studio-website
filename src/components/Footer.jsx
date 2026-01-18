d059906e4a849b2fbf9356b11a45e916cb7c0c02
Mutlu Kurt <81134748+mutlukurt@users.noreply.github.com>
2026-01-18 16:39:47 +0300

    chore: Update footer to 2026 and add License info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footer-bottom">
-                    <p>&copy; 2024 Inked Tattoo Studio. All rights reserved.</p>
+                    <p>&copy; 2026 Mutlu Kurt. Licensed under MIT.</p>
                 </div>
             </div>
         </footer>
